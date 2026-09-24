1d7ba3e229c0dda8c2de010e289a14797152d9d7
zlodag <11688686+zlodag@users.noreply.github.com>
2026-09-24 13:09:48 +0000

    Update package dependencies and modify resource profile URLs

--- src/pages/resource/utils.ts
+++ src/pages/resource/utils.ts
@@ -3,7 +3,8 @@
 import type { ResourceType } from '@medplum/fhirtypes';
 
 export const RESOURCE_PROFILE_URLS: Partial<Record<ResourceType, string>> = {
-  Patient: 'http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient',
+  // Patient: 'http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient',
+  Patient: 'http://hl7.org.nz/fhir/StructureDefinition/NzPatient',  
   ServiceRequest: 'http://medplum.com/StructureDefinition/medplum-provider-lab-procedure-servicerequest',
   Device: 'http://hl7.org/fhir/us/core/StructureDefinition/us-core-implantable-device',
 };
